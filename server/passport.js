c51d5f3a8fd456196f8725e949d90c3e74206a59
Muhammad Abdullah <abdulla152535@gmail.com>
2023-12-17 14:33:13 +0500

    Oauth2

--- server/passport.js
+++ server/passport.js
@@ -29,32 +29,7 @@ passport.use(
             console.error("Authentication Error:", error);
             return done(error); // Pass the error to the done callback
           }
-    const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const jwt = require('jsonwebtoken');
-const createUser = require('./utils/user/createUser');
-
-passport.use(
-  new GoogleStrategy(
-    {
-      clientID: process.env.CLIENT_ID,
-      clientSecret: process.env.CLIENT_SECRET_KEY,
-      callbackURL: 'http://localhost:5000/auth/google/callback',
-    },
-    async (accessToken, refreshToken, profile, done) => {
-      try {
-        const user = await createUser(profile);
-        const token = jwt.sign({ user }, process.env.JWT_SECRET, { expiresIn: '1h' });
-        console.log("Authentication Successful");
-        return done(null, { token });
-      } catch (error) {
-        console.error("Authentication Error:", error);
-        return done(error);
-      }
-    }
-  )
-);
-
+    
     }
   )
 );
