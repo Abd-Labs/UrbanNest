273ab1fdbdc2d1574c7eaef2290b0f9758fec6d1
Muhammad Abdullah <abdulla152535@gmail.com>
2024-04-26 17:04:45 +0500

    minor change

--- client/src/components/utils/generateApiUrl.jsx
+++ client/src/components/utils/generateApiUrl.jsx
@@ -2,7 +2,7 @@
 
 export default function getApiUrl (path)  {
       // Use window.location.origin as domain
-      return `${window.location.origin}${path}`;
+      return `${process.env.REACT_APP_API_DOMAIN}${path}`;
     
   };
   
